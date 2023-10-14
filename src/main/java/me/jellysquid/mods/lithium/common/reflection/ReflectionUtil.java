35ccab355010051b3c55dd50bc3777742f4c399d
dima_dencep <dima_dencep@constructlegacy.ru>
2023-10-14 09:52:10 +0700

    Fix crash reported in Discord

--- src/main/java/me/jellysquid/mods/lithium/common/reflection/ReflectionUtil.java
+++ src/main/java/me/jellysquid/mods/lithium/common/reflection/ReflectionUtil.java
@@ -6,7 +6,11 @@ import net.minecraft.util.crash.CrashReportSection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.regex.Pattern;
+
 public class ReflectionUtil {
+    private static final Pattern pattern = Pattern.compile("Attempted to load class ([A-Za-z]+(/[A-Za-z]+)+) for invalid dist [A-Za-z0-9]+_[A-Za-z0-9]+", Pattern.CASE_INSENSITIVE);
+
     public static boolean hasMethodOverride(Class<?> clazz, Class<?> superclass, boolean fallbackResult, String methodName, Class<?>... methodArgs) {
         while (clazz != null && clazz != superclass && superclass.isAssignableFrom(clazz)) {
             try {
@@ -20,12 +24,16 @@ public class ReflectionUtil {
                         " getting declared methods crashes with NoClassDefFoundError: " + error.getMessage() +
                         ". This is usually caused by modded" +
                         " entities declaring methods that have a return type or parameter type that is annotated" +
-                        " with @Environment(value=EnvType.CLIENT). Loading the type is not possible, because" +
+                        " with @OnlyIn(Dist.CLIENT). Loading the type is not possible, because" +
                         " it only exists in the CLIENT environment. The recommended fix is to annotate the method with" +
                         " this argument or return type with the same annotation." +
                         " Lithium handles this error by assuming the class cannot be included in some optimizations.");
                 return fallbackResult;
             } catch (Throwable e) {
+                if (pattern.matcher(e.getMessage()).matches()) {
+                    return false;
+                }
+
                 final String crashedClass = clazz.getName();
                 CrashReport crashReport = CrashReport.create(e, "Lithium Class Analysis");
                 CrashReportSection crashReportSection = crashReport.addElement(e.getClass().toString() + " when getting declared methods.");
