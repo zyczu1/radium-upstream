9f60fc678fc09f687a8d0f855a0ca9e24b995448
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-02-27 19:53:47 -0500

    Re-enable POI mixin

--- src/main/java/me/jellysquid/mods/lithium/mixin/ai/poi/PointOfInterestTypesMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/poi/PointOfInterestTypesMixin.java
@@ -1,26 +1,30 @@
 package me.jellysquid.mods.lithium.mixin.ai.poi;
 
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import me.jellysquid.mods.lithium.common.world.interests.types.PointOfInterestTypeHelper;
 import net.minecraft.block.BlockState;
-import net.minecraft.world.poi.PointOfInterestTypes;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.poi.PointOfInterestType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Set;
+import java.util.Map;
 
 /**
  * Replaces the backing map type with a faster collection type which uses reference equality.
  */
-@Mixin(PointOfInterestTypes.class)
+@Mixin(targets = {"net/neoforged/neoforge/registries/NeoForgeRegistryCallbacks$PoiTypeCallbacks"})
 public class PointOfInterestTypesMixin {
-    //@Shadow
-    //@Final
-    //protected static Set<BlockState> f_218067_;
+    @Shadow
+    @Final
+    @Mutable
+    static Map<BlockState, RegistryEntry<PointOfInterestType>> BLOCKSTATE_TO_POI_TYPE_MAP;
 
     static {
-        // POI_STATES_TO_TYPE = new Reference2ReferenceOpenHashMap<>(POI_STATES_TO_TYPE); TODO why it broke?
+        BLOCKSTATE_TO_POI_TYPE_MAP = new Reference2ReferenceOpenHashMap<>(BLOCKSTATE_TO_POI_TYPE_MAP);
 
-        // PointOfInterestTypeHelper.init(f_218067_);
+        PointOfInterestTypeHelper.init(BLOCKSTATE_TO_POI_TYPE_MAP.keySet());
     }
 }
